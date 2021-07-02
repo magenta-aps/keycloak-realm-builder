e76d9dfcc2afd0f6f7962e734bde31d8ea04d800
Andreas Kring <andreas@magenta.dk>
2021-07-02 14:50:07 +0200

    Toggle user section based on ENV flag

---
+++ filters.py
@@ -0,0 +1,2 @@
+def string_to_bool(arg: str):
+    return True if arg.lower() == 'true' else False
--- populate-template.sh
+++ populate-template.sh
@@ -4,5 +4,7 @@
 pip install -r requirements.txt
 
 # Populate Keycloak realm JSON template with data from ENV file
-j2 keycloak-realm.json.j2 keycloak.env
+j2 --filters=filters.py keycloak-realm.json.j2 keycloak.env > realm.json
 
+# Populate keycloak.json template with data from ENV file
+j2 --filters=filters.py keycloak.json.j2 keycloak.env > keycloak.json
