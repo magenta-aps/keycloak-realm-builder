61614204d214d57d3a78e66d90424dab7a1f42ff
Casper V. Kristensen <vk@magenta.dk>
2022-05-26 18:38:57 +0200

    [#50496] Delete integration-builder

--- integration/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# SPDX-FileCopyrightText: Magenta ApS
-#
-# SPDX-License-Identifier: MPL-2.0
-
-terraform init -backend-config="conn_str=$POSTGRES_CONNECTION_STRING" -backend-config="schema_name=$POSTGRES_SCHEMA_NAME"
-terraform apply -auto-approve
