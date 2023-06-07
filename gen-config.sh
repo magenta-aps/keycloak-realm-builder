6542e5fb2b723c236c12c6ef2341b1b1be9a2ba2
Casper V. Kristensen <vk@magenta.dk>
2023-06-07 13:12:26 +0200

    feat: [#56381] migrate runfile.sh from adfs-files repo

---
+++ gen-config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: Magenta ApS
+# SPDX-License-Identifier: MPL-2.0
+
+KEYCLOAK_FILE=$1
+
+TOKEN_REPLY=$(curl -s -d "client_id=admin-cli" -d "username=admin" -d "password=admin" -d "grant_type=password" "http://localhost:5000/auth/realms/master/protocol/openid-connect/token")
+TOKEN=$(echo "${TOKEN_REPLY}" | jq .access_token -r)
+JSON_REPLY=$(curl -s -H "Authorization: Bearer $TOKEN" -FproviderId=saml -Ffile=@"${KEYCLOAK_FILE}"  "http://localhost:5000/auth/admin/realms/mo/identity-provider/import-config")
+echo "${JSON_REPLY}" | jq . --sort-keys
