4ce6a9b1c4bf3b8bf3069d51bc423734cf2053a7
Casper V. Kristensen <vk@magenta.dk>
2023-12-05 13:56:24 +0100

    fix [#58815] don't lock terraform 🤠

--- integration/run.sh
+++ integration/run.sh
@@ -3,5 +3,13 @@
 #
 # SPDX-License-Identifier: MPL-2.0
 
-terraform init -backend-config="conn_str=$POSTGRES_CONNECTION_STRING" -backend-config="schema_name=$POSTGRES_SCHEMA_NAME"
-terraform apply -auto-approve
+# We don't lock the terraform workspace to avoid bugs such as
+#  - https://github.com/hashicorp/terraform/issues/33217
+#  - https://github.com/hashicorp/terraform/issues/33217
+#  - https://github.com/hashicorp/terraform/issues/22338
+#  - https://github.com/hashicorp/terraform/pull/26924
+# This assumes that A) we only run one of these init containers per integration and
+# B) that the keycloak client configuration is somewhat static anyway, and
+# as such potential conflicts, should they happen, are not too dangerous anyway.
+terraform init -backend-config="conn_str=$POSTGRES_CONNECTION_STRING" -backend-config="schema_name=$POSTGRES_SCHEMA_NAME" -lock=false
+terraform apply -auto-approve -lock=false
