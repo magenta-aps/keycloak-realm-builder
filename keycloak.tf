e402e84af0c4f7b98e98680d4d31aa60009fce06
Carl Bordum Hansen <carl@bordum.dk>
2025-04-11 13:56:25 +0200

    feat: [#63053] add namespace rbac

--- keycloak.tf
+++ keycloak.tf
@@ -159,10 +159,10 @@ resource "keycloak_realm" "mo" {
 locals {
   # TODO: you can remove "auditlog" once #64270 is deploed everywhere
   collections = [
-    "address", "association", "auditlog", "accesslog", "class", "configuration",
-    "employee", "engagement_association", "engagement", "facet", "file",
-    "health", "itsystem", "ituser", "kle", "leave", "listener", "manager",
-    "owner", "org",
+    "address", "association", "auditlog", "accesslog", "class",
+    "configuration", "employee", "engagement_association", "engagement",
+    "event_namespace", "facet", "file", "health", "itsystem", "ituser", "kle",
+    "leave", "listener", "manager", "owner", "org",
     # TODO: You can remove "role" once #59798 is deployed everywhere
     "org_unit", "registration", "related_unit", "role", "rolebinding", "version"
   ]
