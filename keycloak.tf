1087dcc541f8cf9b6915adb9b343e6d5ae8ba241
Emil <emil@magenta.dk>
2025-07-03 16:02:46 +0200

    docs: [#64963] restyle collection removal TODOs
    This commit restyles the collection removal TODOs such that the
    collection to be removed is moved to the bottom of the list along with
    its removal comment, making it easier to remove in the future.

--- keycloak.tf
+++ keycloak.tf
@@ -164,14 +164,16 @@ resource "keycloak_realm" "mo" {
 
 # TODO: Fetch these from OS2mo
 locals {
-  # TODO: you can remove "auditlog" once #64270 is deploed everywhere
   collections = [
-    "address", "association", "auditlog", "accesslog", "class",
+    "address", "association", "accesslog", "class",
     "configuration", "employee", "engagement_association", "engagement",
     "event", "event_listener", "event_namespace", "facet", "file", "health",
     "itsystem", "ituser", "kle", "leave", "manager", "owner", "org",
+    "org_unit", "registration", "related_unit", "rolebinding", "version",
+    # TODO: You can remove "auditlog" once #64270 is deployed everywhere
+    "auditlog", 
     # TODO: You can remove "role" once #59798 is deployed everywhere
-    "org_unit", "registration", "related_unit", "role", "rolebinding", "version"
+    "role", 
   ]
   permission_types = [
     "read", "create", "update", "terminate", "delete", "refresh"
